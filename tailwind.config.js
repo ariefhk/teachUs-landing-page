42cc4d678c586ddb0fbbee222af49bba603a80dd
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-24 16:22:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      // padding: "20px",
+    },
+    fontFamily: {
+      sans: "Poppins",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
